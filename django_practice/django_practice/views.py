5e0e316f8cf74e4087658c2155b52de1e7535031
DamiennFl <damien.flutre@gmail.com>
2024-06-25 11:11:35 -0700

    homepage created

--- django_practice/django_practice/views.py
+++ django_practice/django_practice/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, "homepage/homepage.html")
+    return render(request, "django_practice\\templates\\homepage\\homepage.html")
--- django_practice/polls/views.py
+++ django_practice/polls/views.py
@@ -1,5 +1,3 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
